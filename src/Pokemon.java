301e5019a7256e9a7037dc584c4cfd64b1f7ebc8
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-09 07:29:36 -0700

    Updated.

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,9 +4,10 @@ public class Pokemon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pikachu welcomes you to the world of Pokemon");
-		System.out.println("\\_/");
-		System.out.println("o^.^");
+		System.out.println("(\\_/)");
+		System.out.println("(o^.^)");
 	    System.out.println("z(_(\")(\")");
+//updated
 
 	}
 
